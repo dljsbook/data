595b77a90a3f1a4f80100fd8158aff7b64b7588b
Kevin Scott <kevin@hitheory.com>
2018-12-08 15:28:51 -0500

    Export a umd build

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,8 @@ export default {
   input: './src/index.ts',
   output: {
     file: 'dist/index.js',
-    format: 'esm',
+    format: 'umd',
+    name: 'dljsbookData',
   },
   plugins: [
     typescript(),
